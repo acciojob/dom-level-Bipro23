49ac762da610745d3e863e4385d0e24d067c46c8
Bipro23 <88981554+Bipro23@users.noreply.github.com>
2023-09-25 19:21:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+//your JS code here. If required.
+	let levelElement = document.getElementById("level");
+	let i=1;
+	while(true){
+		if(levelElement.tagName === "HTML"){
+			break;
+		}else{
+			levelElement = levelElement.parentNode;
+			i++;
+		}
+	}
+	alert(`The level of the element is:  ${i}`);
